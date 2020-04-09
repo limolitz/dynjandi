95817db62ffa8e9acd6daab82a8cc6ca08892116
wasMitNetzen <irgend@wasmitnetzen.de>
2020-04-10 00:01:24 +0200

    Put mask calculation in its own thread

--- get_diff.py
+++ get_diff.py
@@ -8,6 +8,8 @@ from datetime import datetime, timezone, timedelta
 import time
 import numpy as np
 import subprocess as sp
+from threading import Thread
+from queue import Queue
 # based on
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
 
@@ -45,89 +47,108 @@ command = ['ffmpeg',
     '/dev/video4']
 proc = sp.Popen(command, stdin=sp.PIPE,stderr=sp.PIPE,shell=False)
 
+
+class WorkerThread(Thread):
+   def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.queue = Queue()
+       self.result = None
+
+   def run(self):
+       while True:
+            # Wait for next message
+            message = self.queue.get()
+
+            start = datetime.now(timezone.utc)
+
+            # compare images
+            # https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
+            (score, diff) = structural_similarity(message[0], message[1], full=True, multichannel=True)
+            diff = (diff * 255).astype("uint8")
+            ssim_time = datetime.now(timezone.utc)
+
+            b,g,r = cv2.split(diff)
+
+            # blue
+            thresh = cv2.threshold(b, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            color_mask_b = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+            #color_mask_b = cv2.medianBlur(color_mask_b, 11)
+            masked_b = cv2.bitwise_and(live, color_mask_b)
+
+            # green
+            thresh = cv2.threshold(g, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            color_mask_g = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+            #color_mask_g = cv2.medianBlur(color_mask_g, 11)
+            masked_g = cv2.bitwise_and(live, color_mask_g)
+
+            # red
+            thresh = cv2.threshold(r, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            color_mask_r = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+            #color_mask_r = cv2.medianBlur(color_mask_r, 11)
+            masked_r = cv2.bitwise_and(live, color_mask_r)
+
+            mask_or = cv2.bitwise_or(color_mask_b, color_mask_g, color_mask_r)
+            masked_or = cv2.bitwise_and(live, mask_or)
+
+            # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contour_features/py_contour_features.html#
+            contours,hierarchy = cv2.findContours(cv2.cvtColor(mask_or, cv2.COLOR_BGR2GRAY).copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            largest_contour = None
+            area = 0
+            for cnt in contours:
+                #print(cv2.moments(cnt))
+                if cv2.contourArea(cnt) > area:
+                    largest_contour = cnt
+                    area = cv2.contourArea(cnt)
+
+            #hull = cv2.convexHull(largest_contour)
+
+            contour = green.copy()
+            contour_mask = np.zeros(base.shape[:2], np.uint8)
+
+            # https://stackoverflow.com/a/50022122
+            contour = cv2.drawContours(contour, [largest_contour], -1, (255, 255, 255), -1)
+            contour_mask = cv2.drawContours(contour_mask, [largest_contour], -1, 255, -1)
+
+            # Display the resulting frame
+            #cv2.imshow('base', base)
+            #cv2.imshow('base blur', base_blur)
+            #cv2.imshow('live blur', live_blur)
+            #cv2.imshow('contour mask', img)
+            #cv2.imshow('masked or', masked_or)
+            #cv2.imshow('masked contour', masked_contour)
+            cv2.imwrite("contour.png", contour)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            now = datetime.now(timezone.utc)
+            timediff = now - start
+            ssim = ssim_time - start
+            rest = now - ssim_time
+            print(
+                f"Processing took {timediff.total_seconds()*1000:.0f} ms, "
+                f"ssim {ssim.total_seconds()*1000:.0f}, "
+                f"the rest {rest.total_seconds()*1000:.0f}."
+            )
+
+            self.result = (contour, contour_mask)
+
+   def put_message(self, message):
+       self.queue.put(message)
+
+
+worker_thread = WorkerThread()
+worker_thread.start()
+
+ret, live = cap.read()
+worker_thread.put_message((live, base))
+
 while(True):
-    start = datetime.now(timezone.utc)
     ret, live = cap.read()
-    time_reading = datetime.now(timezone.utc)
-    timediff = time_reading - start
-    #cv2.imshow('live', live)
-    #proc.stdin.write(live.tostring())
 
-
-    if start - last_mask_run > MASK_SPEED:
-
-        # compare images
-        # https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
-        (score, diff) = structural_similarity(live, base, full=True, multichannel=True)
-        diff = (diff * 255).astype("uint8")
-        ssim_time = datetime.now(timezone.utc)
-
-        b,g,r = cv2.split(diff)
-
-        # blue
-        thresh = cv2.threshold(b, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        color_mask_b = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-        #color_mask_b = cv2.medianBlur(color_mask_b, 11)
-        masked_b = cv2.bitwise_and(live, color_mask_b)
-
-        # green
-        thresh = cv2.threshold(g, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        color_mask_g = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-        #color_mask_g = cv2.medianBlur(color_mask_g, 11)
-        masked_g = cv2.bitwise_and(live, color_mask_g)
-
-        # red
-        thresh = cv2.threshold(r, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        color_mask_r = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-        #color_mask_r = cv2.medianBlur(color_mask_r, 11)
-        masked_r = cv2.bitwise_and(live, color_mask_r)
-
-        mask_or = cv2.bitwise_or(color_mask_b, color_mask_g, color_mask_r)
-        masked_or = cv2.bitwise_and(live, mask_or)
-
-        # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contour_features/py_contour_features.html#
-        contours,hierarchy = cv2.findContours(cv2.cvtColor(mask_or, cv2.COLOR_BGR2GRAY).copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        largest_contour = None
-        area = 0
-        for cnt in contours:
-            #print(cv2.moments(cnt))
-            if cv2.contourArea(cnt) > area:
-                largest_contour = cnt
-                area = cv2.contourArea(cnt)
-
-        #hull = cv2.convexHull(largest_contour)
-
-        contour = green.copy()
-        contour_mask = np.zeros(base.shape[:2], np.uint8)
-
-        # https://stackoverflow.com/a/50022122
-        contour = cv2.drawContours(contour, [largest_contour], -1, (255, 255, 255), -1)
-        contour_mask = cv2.drawContours(contour_mask, [largest_contour], -1, 255, -1)
-
-        # Display the resulting frame
-        #cv2.imshow('base', base)
-        #cv2.imshow('base blur', base_blur)
-        #cv2.imshow('live blur', live_blur)
-        #cv2.imshow('contour mask', img)
-        #cv2.imshow('masked or', masked_or)
-        #cv2.imshow('masked contour', masked_contour)
-        cv2.imwrite("contour.png", contour)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        now = datetime.now(timezone.utc)
-        timediff = now - start
-        reading = time_reading - start
-        ssim = ssim_time - time_reading
-        rest = now - ssim_time
-        print(
-            f"Processing took {timediff.total_seconds()*1000:.0f} ms, "
-            f"reading {reading.total_seconds()*1000:.0f}, "
-            f"ssim {ssim.total_seconds()*1000:.0f}, "
-            f"the rest {rest.total_seconds()*1000:.0f}.\n"
-        )
-
-        last_mask_run = datetime.now(timezone.utc)
+    if worker_thread.result is not None:
+        contour, contour_mask = worker_thread.result
+        worker_thread.result = None
+        worker_thread.put_message((live, base))
     if contour is not None:
         masked_contour = cv2.bitwise_and(live, contour, mask=contour_mask)
         #cv2.imshow('masked contour', masked_contour)
